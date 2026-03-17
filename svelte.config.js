274f8844f5373f61efdd7fc9da565e66585d1a69
elijah <tomelijah349@gmail.com>
2026-03-17 18:01:52 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs20',
+			regions: ['fra1']  // Match backend region (Frankfurt, good for Nigeria/Europe)
+		})
 	}
 };
 
